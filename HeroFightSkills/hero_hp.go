c4b32397e665b59a5bd6f81fe146b784c0463731
Ася Бирюкова <asyabirukova@Noutbuk-Asa.local>
2024-11-27 17:14:12 +0300

    feat HeroHP

--- HeroFightSkills/hero_hp.go
+++ HeroFightSkills/hero_hp.go
@@ -1,16 +1,23 @@
 package herofightskills
 
 import (
-	"strconv"
-
 	char "github.com/AsyaBiryukova/DND_RandomCharacter/HeroCharsAndSkills"
+	random "github.com/AsyaBiryukova/DND_RandomCharacter/MainHeroInfo"
 )
 
-func Bard_HP() string { // Функция, определяющая здоровье героя
-	bard_conctitution_modifier := char.Constitution_Modifiers() // Вызов итогового модификатора Выносливости
-	bard_hill_dice := 8                                         // Кость здоровья барда 1d8
-	max_bard_hp := bard_conctitution_modifier + bard_hill_dice  // Максимальное здоровья Барда на 1 уровне = максимальное значение кости здоровья + модификатор Выносливости
+func HeroHP() int {
+	hero_class := random.RandomClass()
+	var hero_hp int
 
-	bard_hp_info := "Ваше максимальное здоровье: " + strconv.Itoa(max_bard_hp) // Сообщение с информацией здоровья
-	return bard_hp_info
+	switch {
+	case hero_class == "Волшебник" || hero_class == "Чародей":
+		hero_hp = 6 + char.Constitution_Modifiers()
+	case hero_class == "Бард" || hero_class == "Друид" || hero_class == "Жрец" || hero_class == "Колдун" || hero_class == "Монах" || hero_class == "Плут":
+		hero_hp = 8 + char.Constitution_Modifiers()
+	case hero_class == "Воин" || hero_class == "Паладин" || hero_class == "Следопыт":
+		hero_hp = 10 + char.Constitution_Modifiers()
+	case hero_class == "Варвар":
+		hero_hp = 12 + char.Constitution_Modifiers()
+	}
+	return hero_hp
 }
